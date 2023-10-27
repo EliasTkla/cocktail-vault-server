ead6773147f010cd92ca50c7e3d59038091cfea5
Elias Tokola <eliastokola88@gmail.com>
2023-10-27 01:02:08 -0400

    updated db config pool

--- config/db.js
+++ config/db.js
@@ -1,13 +1,18 @@
 const Pool = require("pg").Pool;
 require("dotenv").config();
 
+//local developement
+// const pool = new Pool({
+//     user: process.env.DB_USER,
+//     password: process.env.DB_PASSWORD,
+//     host: process.env.DB_HOST,
+//     port: process.env.DB_PORT,
+//     database: process.env.DB_NAME
+// });
+
 const pool = new Pool({
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    database: process.env.DB_NAME
-});
+    connectionString: process.env.POSTGRES_URL,
+})
 
 pool.connect((err) => {
     if (err) {
