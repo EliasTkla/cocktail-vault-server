e47ec28f5af5012358c0aa734a4e38a04dc38b7b
Elias Tokola <eliastokola88@gmail.com>
2023-10-27 00:53:18 -0400

    changed db cofig

--- config/db.js
+++ config/db.js
@@ -1,18 +1,13 @@
 const Pool = require("pg").Pool;
 require("dotenv").config();
 
-//local developement
-// const pool = new Pool({
-//     user: process.env.DB_USER,
-//     password: process.env.DB_PASSWORD,
-//     host: process.env.DB_HOST,
-//     port: process.env.DB_PORT,
-//     database: process.env.DB_NAME
-// });
-
 const pool = new Pool({
-    connectionString: process.env.POSTGRES_URL + "?sslmode=require",
-})
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    database: process.env.DB_NAME
+});
 
 pool.connect((err) => {
     if (err) {
