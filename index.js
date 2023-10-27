4799e47b28d9e67a4d0252cba5ec0efd7be92ee8
Elias Tokola <eliastokola88@gmail.com>
2023-10-27 11:25:21 -0400

    removed vercel methods and headers, added on cors

--- index.js
+++ index.js
@@ -6,7 +6,8 @@ require("dotenv").config();
 //cors setup
 var corsOptions = {
     origin: 'https://cocktail-vault.vercel.app/',
-    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'HEAD', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Origin', 'X-Requested-With', 'Accept', 'x-client-key', 'x-client-token', 'x-client-secret', 'Authorization'],
     optionSuccessStatus: 200
 };
 
